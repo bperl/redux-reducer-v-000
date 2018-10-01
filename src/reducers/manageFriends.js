e9ddb04fd97439b50ae71e4898cc9a6718c5e12b
zali <betzalelperlow@gmail.com>
2018-10-01 19:12:30 -0400

    fixed

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -5,9 +5,9 @@ export function manageFriends(state, action){
         case "ADD_FRIEND":
             return { friends: [...state.friends, action.friend] };
         case "REMOVE_FRIEND":
-            const newFriends = [...state.friends];
-            newFriends = newFriends.filter(friend => friend.id !== action.id);
-            return { friends: newFriends };
+            let friends = [...state.friends];
+            friends = friends.filter(friend => friend.id !== action.id);
+            return { friends };
         default:
             return state;
     }
